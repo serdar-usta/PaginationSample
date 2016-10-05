0ee6cf91a7e63bf8591c0657c259ece4d783f61d
Serdar Usta <la211018@std.yildiz.edu.tr>
2016-10-06 00:21:25 +0300

    Files OK

---
+++ Pagination/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Pagination
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.cmbPageSize = new System.Windows.Forms.ComboBox();
+            this.lblPageNumber = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 39);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(587, 311);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(524, 356);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 1;
+            this.btnNext.Text = ">";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(443, 357);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(75, 23);
+            this.btnPrevious.TabIndex = 2;
+            this.btnPrevious.Text = "<";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // cmbPageSize
+            // 
+            this.cmbPageSize.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbPageSize.FormattingEnabled = true;
+            this.cmbPageSize.Location = new System.Drawing.Point(478, 12);
+            this.cmbPageSize.Name = "cmbPageSize";
+            this.cmbPageSize.Size = new System.Drawing.Size(121, 21);
+            this.cmbPageSize.TabIndex = 3;
+            this.cmbPageSize.SelectedIndexChanged += new System.EventHandler(this.cmbPageSize_SelectedIndexChanged);
+            // 
+            // lblPageNumber
+            // 
+            this.lblPageNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblPageNumber.Location = new System.Drawing.Point(371, 10);
+            this.lblPageNumber.Name = "lblPageNumber";
+            this.lblPageNumber.Size = new System.Drawing.Size(100, 21);
+            this.lblPageNumber.TabIndex = 4;
+            this.lblPageNumber.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 416);
+            this.Controls.Add(this.lblPageNumber);
+            this.Controls.Add(this.cmbPageSize);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.ComboBox cmbPageSize;
+        private System.Windows.Forms.Label lblPageNumber;
+    }
+}
+
---
+++ Pagination/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pagination
+{
+    public partial class Form1 : Form
+    {
+        int _pageNumber = 1;
+        int _pageSize;
+        int _totalPageCount = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbPageSize.Items.Add(10);
+            cmbPageSize.Items.Add(25);
+            cmbPageSize.Items.Add(50);
+            cmbPageSize.SelectedItem = _pageSize;
+            //cmbPageSize.SelectedIndex = 0;
+
+            _totalPageCount = GetTotalCount(_pageSize);
+
+            //lblPageNumber.Text = string.Format("{0} / {1}", _pageNumber.ToString(), _totalPageCount);
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (_pageNumber + 1 <= _totalPageCount)
+            {
+                _pageNumber++;
+
+                BindGridData(_pageSize, _pageNumber);
+
+                lblPageNumber.Text = string.Format("{0} / {1}", _pageNumber.ToString(), _totalPageCount);
+            }
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            if (_pageNumber - 1 > 0)
+            {
+                _pageNumber--;
+
+                BindGridData(_pageSize, _pageNumber);
+
+                lblPageNumber.Text = string.Format("{0} / {1}", _pageNumber.ToString(), _totalPageCount);
+            }
+        }
+
+        private void cmbPageSize_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbPageSize.SelectedIndex > -1)
+            {
+                _pageSize = (int)cmbPageSize.SelectedItem;
+                _pageNumber = 1;
+                _totalPageCount = GetTotalCount(_pageSize);
+
+                BindGridData(_pageSize, _pageNumber);
+
+                lblPageNumber.Text = string.Format("{0} / {1}", _pageNumber.ToString(), _totalPageCount);
+            }
+        }
+
+        void BindGridData(int pageSize, int pageNumber)
+        {
+            int skip = (pageNumber - 1) * pageSize;
+            int take = pageSize;
+
+            NorthwindContext context = new NorthwindContext();
+
+
+
+            dataGridView1.DataSource = context
+            .Products
+            .Select(p => new { p.ProductID, p.ProductName, p.Category.CategoryName, p.UnitPrice })
+            .OrderBy(p => p.ProductID)
+            .Skip(skip)
+            .Take(take)
+            .ToList();
+        }
+
+        int GetTotalCount(int pageSize)
+        {
+            int totalProductsCount = 0;
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                totalProductsCount = context.Products.Count();
+            }
+            int totalPageCount = (int)Math.Ceiling((double)totalProductsCount / pageSize);
+            return totalPageCount;
+        }
+    }
+}
